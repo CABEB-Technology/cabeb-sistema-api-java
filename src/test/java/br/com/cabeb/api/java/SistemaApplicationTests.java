5ac72f31886ad9e79ce58f7ee8f696a07be00ff5
Brendson <brendson.net@gmail.com>
2022-10-01 14:34:47 -0300

    feat: criando projeto backend

---
+++ src/test/java/br/com/cabeb/api/java/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cabeb.api.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
