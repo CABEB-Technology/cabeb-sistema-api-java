da1e4eda2f9786a6a8cb05354a04c0074eb96163
Brendson <brendson.net@gmail.com>
2022-10-01 17:05:15 -0300

    feat: criada classes de exceptions

---
+++ src/main/java/br/com/cabeb/api/java/exception/AbstractException.java
@@ -0,0 +1,17 @@
+package br.com.cabeb.api.java.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class AbstractException extends RuntimeException {
+
+    private String erro;
+    private String descricao;
+
+    public AbstractException(String erro, String descricao) {
+        this.erro = erro;
+        this.descricao = descricao;
+    }
+}
